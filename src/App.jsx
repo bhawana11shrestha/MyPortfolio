86a840e2ac58b4a13a2ef63e3d5bf3a830c294da
unknown <bhawana11shrestha>
2024-02-14 14:21:25 +0545

    Hero is created i.e me

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import styles from './App.module.css';
+import { Hero } from './compontents/Hero/Hero';
 import { Navbar } from './compontents/Navbar/Navbar';
 
 function App() {
  
 
   return <div className={styles.App}> 
-  <Navbar/>
+  <Navbar />
+  <Hero />
   </div>;
     
 }
---
+++ src/compontents/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi I'am Bhawana 👩🏻</h1>
+        <p className={styles.description}>
+          I am a Backend Developer with 2 years of experience in Python Django.
+          Reach out if you'd like to know more!
+        </p>
+        <a href="mailto:bhawanastha9999@gmail.com" className={styles.contactBtn}>Contact Me</a>
+      </div>
+      <img src={getImageUrl("hero/heroImage.png")} alt="Image of me" className={styles.heroImg}/>
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
