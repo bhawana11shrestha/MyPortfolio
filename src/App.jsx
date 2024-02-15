60d0a7c14db61893c12820769408c4dbc0a6d045
unknown <bhawana11shrestha>
2024-02-16 01:12:45 +0545

    Experience section created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import styles from "./App.module.css";
 import { About } from "./components/About/About";
+import {Experience} from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
+
 
 
 function App() {
@@ -10,8 +12,9 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Experience />
     </div>
   );
-}
+} 
 
 export default App;
