ff89f8e9e502527d0b382db69612a22340ab97ec
unknown <bhawana11shrestha>
2024-02-16 16:29:11 +0545

    Projects section created

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -28,11 +28,11 @@ export const Experience = () => {
               <li key={id} className={styles.historyItem}>
                 <img
                   src={getImageUrl(historyItem.imageSrc)}
-                  alt={`${historyItem.organisation}Logo`}
+                  alt={`${historyItem.organisation} Logo`}
                 />
                 <div className={styles.historyItemDetails}>
-                  <h3>{`${historyItem.role},${historyItem.organisation}`}</h3>
-                  <p>{`${historyItem.startDate},${historyItem.endDate}`}</p>
+                  <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                  <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
                   <ul>
                     {historyItem.experiences.map((experience, id) => {
                       return <li key={id}>{experience}</li>;
